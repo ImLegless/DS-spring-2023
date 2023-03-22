5baf04066b5ea24147ffc11a3ff760861ec2d194
ImLegless <86304879+ImLegless@users.noreply.github.com>
2023-03-22 12:47:13 -0500

    update homework

--- Code/Code/IntArrayBag.java
+++ Code/Code/IntArrayBag.java
@@ -1,62 +1,71 @@
 package Code;
 
 public class IntArrayBag {
-    //instance variables
-    private int numItems; //the number of current items in the bag
-                          //0<=numItems<=data.length
-    private int [] data; /*storage for the data, valid items are
-                         always in indices 0 to numItems -1*/
+    // instance variables
+    private int numItems; // the number of current items in the bag
+                          // 0<=numItems<=data.length
+    private int[] data; /*
+                         * storage for the data, valid items are
+                         * always in indices 0 to numItems -1
+                         */
 
-    /*default constructor - create an array of default size and 
-    *                       start empty
-    */
-    public IntArrayBag(){
+    /*
+     * default constructor - create an array of default size and
+     * start empty
+     */
+    public IntArrayBag() {
         data = new int[5];
         numItems = 0;
     }
-    //custom contructor
-    public IntArrayBag(int capacity){
+
+    // custom contructor
+    public IntArrayBag(int capacity) {
         data = new int[capacity];
         numItems = 0;
     }
-    //empty() - returns true if no valid items, false otherwise
-    public boolean is_empty(){
+
+    // empty() - returns true if no valid items, false otherwise
+    public boolean is_empty() {
         return numItems == 0;
     }
-    //full()
-    public boolean is_full(){
+
+    // full()
+    public boolean is_full() {
         return numItems == data.length;
     }
-    //contains() - true if value is equal to at least 1 item
-    public boolean contains(int value){
-        for(int i = 0;i<numItems;i++){
-            if(data[i]==value){
+
+    // contains() - true if value is equal to at least 1 item
+    public boolean contains(int value) {
+        for (int i = 0; i < numItems; i++) {
+            if (data[i] == value) {
                 return true;
             }
         }
         return false;
     }
-    //size
-    public int getSize(){
+
+    // size
+    public int getSize() {
         return numItems;
     }
-    //add()
-    public void add(int value){
-        //start with a simple bad that refuses to add when out of room
-        if(is_full()){
+
+    // add()
+    public void add(int value) {
+        // start with a simple bad that refuses to add when out of room
+        if (is_full()) {
             System.out.println("Storage full");
-        }
-        else{
+        } else {
             data[numItems] = value;
             numItems++;
         }
     }
-    //remove()
-    public void remove(int value){
-        if(contains(value)){
-            for(int i = 0;i<numItems;i++){
-                if(data[i]==value){
-                    data[i] = data[numItems -1];
+
+    // remove()
+    public void remove(int value) {
+        if (contains(value)) {
+            for (int i = 0; i < numItems; i++) {
+                if (data[i] == value) {
+                    data[i] = data[numItems - 1];
                     numItems--;
                     break;
                 }
@@ -64,6 +73,16 @@ public class IntArrayBag {
         }
     }
 
+    public void add_many(int[]... values) {
+        for (int i = 0; i < values.length; i++) {
+            add(data[i]);
+        }
+    }
 
+    public void add_all(IntArrayBag other) {
+        for (int i = 0; i < other.numItems; i++) {
+            add(other.data[i]);
+        }
+    }
 
 }
